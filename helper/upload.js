a01e4260f83c43cc6d8ea6c47c59399c9f4d6469
Devantics01 <devantics01@gmail.com>
2025-10-13 02:42:39 +0100

    Update upload.js

--- helper/upload.js
+++ helper/upload.js
@@ -1,14 +1,9 @@
 import multer from 'multer';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = import.meta.url;
-const __dirname = path.dirname(__filename);
-const parentDir = path.resolve(__dirname, '..');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(parentDir, 'public/uploads'));
+        cb(null, '../public/uploads');
     },
     filename: function (req, file, cb) {
         console.log(req.body);
